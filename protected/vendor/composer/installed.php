0b50d483b311617eec6dbe29d0ecc643858ea956
Daria <dariazychkina@gmail.com>
2024-10-28 16:16:05 +0300

    first commit

---
+++ protected/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'yiisoft/yii' => array(
+            'pretty_version' => '1.1.14',
+            'version' => '1.1.14.0',
+            'reference' => 'f0fee98ee84f70f1f3652f65562c9670e919cb4e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../yiisoft/yii',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
